94a742a138430afcdb2bf32120b916ff28c00db4
zzh <1652311561@qq.com>
2020-04-24 11:15:24 +0800

    第一次提交pojo zzh

---
+++ examines/common/src/main/java/com.csdj.examines/pojo/YxAdviseCheckresult.java
@@ -0,0 +1,38 @@
+public class YxAdviseCheckresult {
+    private  Integer relationId;
+    private  Integer resultId;
+    private  String adviseArr;
+
+    @Override
+    public String toString() {
+        return "YxAdviseCheckresult{" +
+                "relationId=" + relationId +
+                ", resultId=" + resultId +
+                ", adviseArr='" + adviseArr + '\'' +
+                '}';
+    }
+
+    public Integer getRelationId() {
+        return relationId;
+    }
+
+    public void setRelationId(Integer relationId) {
+        this.relationId = relationId;
+    }
+
+    public Integer getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(Integer resultId) {
+        this.resultId = resultId;
+    }
+
+    public String getAdviseArr() {
+        return adviseArr;
+    }
+
+    public void setAdviseArr(String adviseArr) {
+        this.adviseArr = adviseArr;
+    }
+}
