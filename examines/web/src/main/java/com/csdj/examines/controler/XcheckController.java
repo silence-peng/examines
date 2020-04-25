3d973f1ac7500c6eb955f7a6c5de206091281116
MR.PENG <2151925612@qq.com>
2020-04-25 20:50:36 +0800

    2

--- examines/web/src/main/java/com/csdj/examines/controler/XcheckController.java
+++ examines/web/src/main/java/com/csdj/examines/controler/XcheckController.java
@@ -23,13 +23,13 @@ public class XcheckController {
 
     /**
      * 判断账户用户是否进行了X射线检查
-     * @param userId 对应账户id
+     * @param userid 对应账户id
      * @param sex 对应账户夫妻中一方的性别 默认男1 女0
      * @return  检查结果
      */
     @RequestMapping("isCheck")
-    public Object isCheck(Integer userId,Integer sex){
-        return service.IsCheckX(userId, sex);
+    public Object isCheck(Integer userid,Integer sex){
+        return service.IsCheckX(userid, sex);
     }
 
     /**
