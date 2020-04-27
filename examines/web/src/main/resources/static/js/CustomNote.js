def2afe39a81be73ea904c0d8f3b97b46de66c79
zzw <1101133895@qq.com>
2020-04-27 12:27:58 +0800

    新增短信系统页面

---
+++ examines/web/src/main/resources/static/js/CustomNote.js
@@ -0,0 +1,30 @@
+layui.use(['form','layer','laydate','jquery','table'], function() {
+	var form = layui.form
+		,layer = layui.layer
+		,laydate = layui.laydate
+		,$ = layui.jquery
+		,table = layui.table;
+		
+		laydate.render({
+		   elem: '#mdate'
+		});
+		laydate.render({
+		   elem: '#fdate'
+		});
+		
+		table.render({
+			elem: '#test',
+			url: '/',
+			cols: [
+				[{field: 'userid',title: '编号',sort: true}
+				, {field: 'fname',title: '姓名'}
+				, {field: 'fcard',title: '身份证号'}
+				, {field: 'fage',title: '年龄'}
+				, {field: 'fbirthdate',title: '出生日期'}
+				, {field: 'address',title: '地址'}
+				, {field: 'fphone',title: '号码'},
+				, {title:'操作', toolbar: '#barDemo'}]
+			],
+			page: true
+		});
+})
